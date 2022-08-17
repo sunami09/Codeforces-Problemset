c0e619a665466732ab6c9f1789a863ac45f6f1b6
sunami09 <sunamidasgupta@gmail.com>
2022-08-16 20:23:50 -0700

    282A - Bit++

---
+++ Bit++.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    int x = 0;
+    while(t--){
+        string s;
+        cin >> s;
+        if(s[1] == '+'){
+            x++;
+        }else{
+            x--;
+        }
+    } 
+
+    cout << x;
+    return 0;
+}
