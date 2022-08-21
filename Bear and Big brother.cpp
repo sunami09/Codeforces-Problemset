6f7e54d90ee44dc871a9479c78f87048b7e5783e
sunami09 <sunamidasgupta@gmail.com>
2022-08-21 12:31:46 -0700

    791A - Bear and Brothers

---
+++ Bear and Big brother.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+
+    int count = 0;
+    while(a <= b){
+        count ++;
+
+        a = a * 3;
+        b = b * 2;
+    }
+
+    cout << count;
+    return 0;
+}
